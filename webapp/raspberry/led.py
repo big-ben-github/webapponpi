38d660813182f4e02180ec59f16613a92bb2a2ca
xubenling <2550266959@qq.com>
2018-11-07 17:01:07 +0800

    fix cant find .so on raspberry-pi

--- webapp/raspberry/led.py
+++ webapp/raspberry/led.py
@@ -2,7 +2,7 @@ from ctypes import CDLL
 
 
 def toggle(status):
-    led = CDLL('./raspberrypiled.so')
+    led = CDLL('./raspberrypiled.so') # may use absolute path when on raspberry-pi
 
     ledstatus = {'OFF': 0, 'ON': 1}
 
